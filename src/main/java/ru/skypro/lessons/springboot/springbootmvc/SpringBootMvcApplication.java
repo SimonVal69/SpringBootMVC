a3066add8babcd192052ac218e8fd259cdddb25d
SimonVal69 <117351123+SimonVal69@users.noreply.github.com>
2023-04-29 13:55:19 +0400

    SwaggerUI has been added for debugging

--- src/main/java/ru/skypro/lessons/springboot/springbootmvc/SpringBootMvcApplication.java
+++ src/main/java/ru/skypro/lessons/springboot/springbootmvc/SpringBootMvcApplication.java
@@ -1,9 +1,11 @@
 package ru.skypro.lessons.springboot.springbootmvc;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class SpringBootMvcApplication {
 
     public static void main(String[] args) {
